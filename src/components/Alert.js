8725088b8bb2f8a4640aa03412d056f58ec12f56
Partha Pratim Sarma <parthasarma34@gmail.com>
2021-07-06 21:44:00 +0530

    Added useEffect

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 const Alert = ({ message,type, removeAlert, list }) => {
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     removeAlert();
-  //   }, 3000);
-  //   return () => clearTimeout(timeout);
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [list]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [list]);
   return <p className={`alert alert-${type}`}>{message}</p>;
 };
 
